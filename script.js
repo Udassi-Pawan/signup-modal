8686388c9f0da33e59b9f8e95817c32c7e9d5cd4
udassi <pawanudassi@hotmail.com>
2023-03-20 23:58:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const a = document.querySelectorAll("input");
+
+// a.forEach(function (el) {
+//   el.addEventListener("focus", function () {
+//     const iska = el.classList[0];
+//     console.log(iska);
+
+//     document.querySelector(".label-" + iska).classList.add("uper");
+//   });
+// });
+
+// a.forEach(function (el) {
+//   el.addEventListener("focusout", function () {
+//     const iska = el.classList[0];
+//     console.log(iska);
+//     document.querySelector(".label-" + iska).classList.remove("uper");
+//   });
+// });
